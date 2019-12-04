d21990594647142d237ce86df1d9d3892863da1e
fanioc <fanioc@163.com>
2019-12-04 16:22:08 +0800

    增加staff登入用例

--- go_house-api/model/staff.go
+++ go_house-api/model/staff.go
@@ -17,9 +17,16 @@ type Staff struct {
 
 func (u *Staff) Login() (check bool, info Staff) {
 	//TODO::连接数据,进行验证
+	if u.Name == "admin" && u.Password == "1234567" {
+		return true, *u
+	}
 	return true, *u
 }
 
 func (u *Staff) ChangeAuditStatus() {
+
+}
+
+func (u *Staff) AddUser() {
 
 }
